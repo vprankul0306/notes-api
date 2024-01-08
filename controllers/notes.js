c06ef831e64507fdb25a2f13eabc828cf8d50ded
Prankul Verma <vprankul@gmail.com>
2024-01-08 21:35:50 +0530

    feature to delete and edit a note is added

--- controllers/notes.js
+++ controllers/notes.js
@@ -63,4 +63,64 @@ const createNote = async (req, res) => {
   }
 };
 
-module.exports = { getNotes, createNote, getNoteById };
+const editNoteById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, note } = req.body;
+
+    if (!title || !note) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+
+    const userId = req.id;
+    const user = await User.findOne({ _id: userId });
+    if (userId && user) {
+      const noteExists = await Note.findOne({ userId });
+      if (noteExists) {
+        const currNote = noteExists.notes.find((note) => note._id == id);
+        if (currNote) {
+          currNote.title = title;
+          currNote.note = note;
+          await noteExists.save();
+          return res.status(200).json({ message: "Note updated successfully" });
+        } else {
+          return res.status(400).json({ message: "Note not found" });
+        }
+      } else {
+        return res.status(400).json({ message: "Note not found" });
+      }
+    } else {
+      return res.status(400).json({ message: "User not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteNoteById = async (req, res) => {
+  const { id } = req.params;
+  const userId = req.id;
+  const user = await User.findOne({ _id: userId });
+
+  if (userId && user) {
+    const noteExists = await Note.findOne({ userId });
+    if (noteExists) {
+      const currNote = noteExists.notes.find((note) => note._id == id);
+      if (currNote) {
+        currNote.deleteOne();
+        await noteExists.save();
+        return res.status(200).json({ message: "Note deleted successfully" });
+      } else {
+        return res.status(400).json({ message: "Note not found" });
+      }
+    }
+  }
+};
+
+module.exports = {
+  getNotes,
+  createNote,
+  getNoteById,
+  editNoteById,
+  deleteNoteById,
+};
--- routes/notes.js
+++ routes/notes.js
@@ -1,13 +1,19 @@
 const notesRouter = require("express").Router();
 const isAuthenticated = require("../middlewares/isAuthenticated");
-const { createNote, getNotes, getNoteById } = require("../controllers/notes");
+const {
+  createNote,
+  getNotes,
+  getNoteById,
+  editNoteById,
+  deleteNoteById,
+} = require("../controllers/notes");
 
 notesRouter.use(isAuthenticated);
 
 notesRouter.get("/api/notes", getNotes);
 notesRouter.get("/api/notes/:id", getNoteById);
 notesRouter.post("/api/notes", createNote);
-notesRouter.put("/api/notes/:id");
-notesRouter.delete("/api/notes/:id");
+notesRouter.put("/api/notes/:id", editNoteById);
+notesRouter.delete("/api/notes/:id", deleteNoteById);
 
 module.exports = notesRouter;
